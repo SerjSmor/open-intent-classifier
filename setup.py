9df0c72ef10ae81a628eb14ada4466d420cea63c
Serj Smor <serjsmor@gmail.com>
2024-09-16 09:45:52 +0300

    Add repo link to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     version=VERSION,
     packages=["open_intent_classifier"],
     author="Serj Smorodinsky",
+    url="https://github.com/SerjSmor/open-intent-classifier",
     author_email="serjsmor@gmail.com",
     description="This library has two purposes: 1. allow to easily test semantic classification with open labels (not pre defined) for intent recognition. "
                 "2. allow to experiment with different n-shot classification components.",
