e5d129b03ff4c32374b4e14b1fd181a3b21d087a
Serj Smor <serjsmor@gmail.com>
2024-09-16 09:36:37 +0300

    Update setup.py with README content

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 from open_intent_classifier import VERSION
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="open_intent_classifier",
@@ -10,5 +13,7 @@ setup(
     author_email="serjsmor@gmail.com",
     description="This library has two purposes: 1. allow to easily test semantic classification with open labels (not pre defined) for intent recognition. "
                 "2. allow to experiment with different n-shot classification components.",
-    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas"]
+    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
