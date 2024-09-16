84eb6aa2f8147fca79fb6e98696b1d5bff17fb1b
Serj Smor <serjsmor@gmail.com>
2024-09-16 08:35:10 +0300

    Add pandas as dependency

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email="serjsmor@gmail.com",
     description="This library has two purposes: 1. allow to easily test semantic classification with open labels (not pre defined) for intent recognition. "
                 "2. allow to experiment with different n-shot classification components.",
-    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers"]
+    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas"]
 )
