034d24cdc4a9c5d80ca5012b7dcdb633fe76d74a
Serj Smor <serjsmor@gmail.com>
2024-11-03 16:42:19 +0200

    Add additional dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ setup(
     author_email="serjsmor@gmail.com",
     description="This library has two purposes: 1. allow to easily test semantic classification with open labels (not pre defined) for intent recognition. "
                 "2. allow to experiment with different n-shot classification components.",
-    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas"],
+    install_requires=["transformers", "torch", "sentencepiece", "scikit-learn", "sentence-transformers", "pandas",
+                      "python-dotenv", "openai"],
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
